be42deec5261530402b66b0be1d0c108c8528e0c
Rishikesh <kumarcrishikesh@gmail.com>
2024-03-21 01:13:04 -0400

    Flake changes to create binary

--- flake.nix
+++ flake.nix
@@ -1,18 +1,36 @@
 {
-  inputs = { 
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=release-23.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShell.x86_64-linux =
-        pkgs.mkShell { 
-          buildInputs = with pkgs; [ 
-          python311Full
-          python311Packages.pyparsing 
-          python311Packages.ipykernel
-          ]; 
-      };
-   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system ;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [ 
+            python311Full
+            python311Packages.pyparsing 
+            python311Packages.ipykernel
+            python311Packages.nuitka
+             ];
+            shellHook = ''
+            source="sql-parser/main.py"
+            binary="main.bin"
+            if ! [ -e "$binary" ]; then
+              echo "Binary does not exist. Building...."
+              nuitka3 --follow-imports sql-parser/main.py
+            else
+              echo "Binary already exists. Build skipped"
+            fi
+            '';
+          };
+        }
+      );
 }
